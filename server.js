5f2c12d6d7b3abf0a8789cfed55a7128a88474a5
Raymond Chen <raymondchen86@hotmail.com>
2019-03-17 03:34:04 -0700

    connect mongodb with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongoDB through mongoose
+mongoose
+    .connect(db)
+    .then(() => 
+        console.log('mongoDB connected')
+    )
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => 
     res.send('hello world')
