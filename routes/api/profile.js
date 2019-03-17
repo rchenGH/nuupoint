6c224cd070cd3a542a48a4490daf3df6a9eb68bf
Raymond Chen <raymondchen86@hotmail.com>
2019-03-17 04:31:42 -0700

    add gitignore, and routes of posts, profiles, and users

---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+// location, bio, experiences, social network links
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  Public
+router.get('/test', (req, res) => res.json({msg: 'profile works'}));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+// routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // DB Config
@@ -17,7 +23,13 @@ app.get('/', (req, res) =>
     res.send('hello world')
 );
 
+// use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 5000;
 
+// listen on port 5000 for connections
 app.listen(port, () => console.log(`Server running on port ${port}`))
 
